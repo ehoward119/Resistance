3ac0271bbddee9608f62017a06a4e31245073e24
jknaudt21 <jknaudt21@cmc.edu>
2020-03-09 00:26:47 -0700

    feat(lobby): Completed lobby functionality

--- resist2/bod.js
+++ resist2/bod.js
@@ -10,7 +10,8 @@ createForm.addEventListener('submit', (e) => {
     if (createForm.name.value != '') {
         code = idGen()
         sessionStorage.setItem('roomID', code)
-        sessionStorage.setItem('hostName', createForm.name.value)
+        sessionStorage.setItem('playerName', createForm.name.value)
+        sessionStorage.setItem('isHost', "yes")
         db.collection('Rooms').doc(code).set({
             isOpen: true,
             numPlayers: 1,
@@ -18,55 +19,101 @@ createForm.addEventListener('submit', (e) => {
             downvoteCounter: 0,
             resistanceScore: 0,
             spyScore: 0
-        }).then(function() {
-        db.collection('Rooms').doc(code).collection("Players").add({
-            name: createForm.name.value,
+        }).then(() =>{
+        db.collection('Rooms').doc(code).collection("Players").doc( createForm.name.value).set({
             rotNumber: 0,
             hasVoted: true,
             isMissionMember: false,
             isResistance: true,
             vote: true
-        }).then(function() {
+        }).then(() => {
             location ="lobby.html";
-        })});
-    }
-});
-
-// joing a room
-joinForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let name = joinForm.name.value;
-    let roomCode = joinForm.code.value;
-    if (roomCode != '' && name != '') {
-        let room = null;
-        db.collection('Rooms').get().then(snapshot => {
-            snapshot.docs.forEach(doc => {
-                if(doc.id == roomCode) {
-                    room = doc;
-                }
-            });
-        
-            if(room == null) {
-                console.log("Invalid Room Code");
-            }
-            else {
-                db.collection("Rooms").doc(roomCode).update({
-                    numPlayers: (room.data().numPlayers + 1),
-                });
-                db.collection("Rooms").doc(roomCode).collection("Players").add({
-                    name: joinForm.name.value,
-                    rotNumber: (room.data().numPlayers),
-                    hasVoted: true,
-                    isMissionMember: false,
-                    isResistance: true,
-                    vote: true
-                });
-                joinForm.name.value = '';
-                joinForm.code.value = '';
-            }
+            })
         });
     }
 });
+
+// Helper to join a room
+joinForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = joinForm.name.value;
+    const roomCode = joinForm.code.value;
+    joinHelp(roomCode, name);
+    joinForm.name.value = '';
+    joinForm.code.value = '';
+});
+
+// helper method for join room
+async function joinHelp(roomCode, name) {
+    if (roomCode != '' && name != '') {
+        const open = await getIsOpen(roomCode);
+        const numPlayers = await getNumPlayers(roomCode);
+        if (roomCode != '' && name != '' && open && numPlayers < 10) {
+            let room = null;
+            db.collection('Rooms').get().then(snapshot => {
+                snapshot.docs.forEach(doc => {
+                    if(doc.id == roomCode) {
+                        room = doc;
+                    }
+                });
+            
+                if(room == null) {
+                    console.log("Invalid Room Code");
+                }
+                else {
+                    db.collection("Rooms").doc(roomCode).update({
+                        numPlayers: (room.data().numPlayers + 1),
+                    }).then(() => {
+                    db.collection('Rooms').doc(roomCode).collection("Players").doc(name).set({
+                        rotNumber: (room.data().numPlayers),
+                        hasVoted: false,
+                        isMissionMember: false,
+                        isResistance: true,
+                        vote: true
+                    }).then( () => {
+                        sessionStorage.setItem('roomID', roomCode)
+                        sessionStorage.setItem('playerName', name)
+                        sessionStorage.setItem('isHost', "no")
+                        location ="lobby.html"
+                    })
+                    });
+                }
+            });
+        } 
+        else {
+            if (numPlayers == 10) {
+                console.log("Too many players!");
+            }
+            else if (!open) {
+                console.log("The game already started!");
+            }
+        } 
+    }
+    else {
+        console.log("Must enter a valid username and room code!");
+    }
+}
+
+
+
+// returns the desired room - useful for getting data from the room
+async function getRoom(roomCode) {
+    const doc = await db.collection('Rooms').doc(roomCode).get();
+    return doc;
+}
+
+
+// getter for isOpen
+async function getIsOpen(roomCode) {
+    const doc = await getRoom(roomCode);
+    return doc.data().isOpen;
+}
+
+// getter for numPlayers
+async function getNumPlayers(roomCode) {
+    const doc = await getRoom(roomCode);
+    return doc.data().numPlayers;
+}
 
 function idGen (){
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
--- resist2/lobby.js
+++ resist2/lobby.js
@@ -1,7 +1,97 @@
-const id = sessionStorage.getItem('roomID')
-const name =sessionStorage.getItem('hostName')
+const roomId = sessionStorage.getItem('roomID')
+const isHost = sessionStorage.getItem('isHost')
+const playerName =sessionStorage.getItem('playerName')
+const playersRef = db.collection('Rooms').doc(roomId).collection('Players')
+const roomRef =  db.collection('Rooms').doc(roomId)
+const startButton = document.getElementById('start')
 
+// -------------------------------------------------------------------------------
+// Simple code that provides the room info
+// Can be deleted if you want, but its useful for debugging
+// -------------------------------------------------------------------------------
 document.getElementById('roomID').innerHTML =
-                'Your Room ID: '+id
+                'Your Room ID: '+roomId
 document.getElementById('hostName').innerHTML = 
-                'The host: '+ name
+                'Your Name: '+ playerName 
+
+                
+// -------------------------------------------------------------------------------
+// Listener for any change on the database (adding players)
+// IDK if this function could be turned into a helper and placed into the function below
+// -------------------------------------------------------------------------------
+playersRef.onSnapshot((snapshot) =>{
+    let playerNames = [];
+    snapshot.docs.forEach(doc => {
+        playerNames.push(doc.id)
+    });
+    updatePlayers(playerNames)
+});
+
+// -------------------------------------------------------------------------------
+// Function that checks if the room is still open or not
+// If the room is not open, this means you should be redirected to the game page
+// -------------------------------------------------------------------------------
+roomRef.onSnapshot((snapshot) =>{
+    if (!snapshot.data().isOpen) {
+        location = "game.html"
+    }
+});
+
+// -------------------------------------------------------------------------------
+// Function for starting the game
+// -------------------------------------------------------------------------------
+startButton.addEventListener('click', () => {
+    // This check might seem dumb but HTML and JavaScript were acting up alll weird
+    if (isHost == "yes") startHelp(roomId)
+    else alert("You're not the host!")
+});
+
+// helper function for starting a game
+async function startHelp(roomCode) {
+    const numPlayers = await getNumPlayers(roomCode);
+    // TEMPORARY CONDITION FOR TESTING, NUMBER OF PLAYERS SHOULD BE 5
+    if (numPlayers >= 2 && numPlayers <= 10) {
+        await roomRef.update({isOpen: false});
+        location = "game.html"
+    }
+    else {
+        alert("Error: Wrong number of players. Must have between 5 and 10 players.");
+    }
+}
+
+
+
+
+
+
+// Helper function that updates the player list
+function updatePlayers(names){ 
+    const playerList = document.getElementById('playerList')
+    clean(playerList);
+    names.forEach((name) =>{
+        var entry = document.createElement("LI");
+        var text = document.createTextNode(name);
+        entry.appendChild(text);
+        playerList.appendChild(entry); 
+    })
+
+}
+
+// Helper function to clean elements
+function clean(node){
+    while (node.firstChild) {
+        node.removeChild(node.lastChild);
+     }
+}
+
+// getter for numPlayers
+async function getNumPlayers(roomCode) {
+    const doc = await getRoom(roomCode);
+    return doc.data().numPlayers;
+}
+
+// returns the desired room - useful for getting data from the room
+async function getRoom(roomCode) {
+    const doc = await db.collection('Rooms').doc(roomCode).get();
+    return doc;
+}
